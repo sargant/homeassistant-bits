78ac289374c7c3075e1b6b653c596ce738786684
Rob Sargant <robert@sargant.com>
2026-08-16 16:32:41 +0100

    Read package archive from stdin explicitly

--- packages/copy-from-git.sh
+++ packages/copy-from-git.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
-git archive HEAD packages | ssh root@assistant 'tar -x -C /config'
+git archive HEAD packages | ssh root@assistant 'tar -xf - -C /config'
