b8d2544cacb1868e65f98ec70219ad24ea55efd7
Rob Sargant <robert@sargant.com>
2026-08-17 14:01:41 +0100

    Make running sensor algorithm agnostic

--- custom_components/calibrated_appliance_monitor/binary_sensor.py
+++ custom_components/calibrated_appliance_monitor/binary_sensor.py
@@ -1,39 +1,34 @@
-"""Dishwasher running sensor."""
+"""Binary sensors exposed by a calibrated appliance monitor."""
 
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass, BinarySensorEntity
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
 
-from . import DishwasherMonitor
-from .const import APPLIANCE_ID, APPLIANCE_MANUFACTURER, APPLIANCE_MODEL, APPLIANCE_NAME, DOMAIN
+from . import CalibratedApplianceMonitorConfigEntry
+from .algorithms.base import ApplianceMonitor
 
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    entry: ConfigEntry,
+    entry: CalibratedApplianceMonitorConfigEntry,
     async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
-    monitor: DishwasherMonitor = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities([RunningSensor(monitor)])
+    """Expose the selected algorithm's running state."""
+    async_add_entities([RunningSensor(entry.runtime_data)])
 
 
 class RunningSensor(BinarySensorEntity):
+    """Simple running / not-running view over the algorithm lifecycle."""
+
     _attr_device_class = BinarySensorDeviceClass.RUNNING
     _attr_has_entity_name = True
     _attr_name = "Running"
     _attr_should_poll = False
-    _attr_unique_id = f"{APPLIANCE_ID}_running"
 
-    def __init__(self, monitor: DishwasherMonitor) -> None:
+    def __init__(self, monitor: ApplianceMonitor) -> None:
         self.monitor = monitor
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, APPLIANCE_ID)},
-            name=APPLIANCE_NAME,
-            manufacturer=APPLIANCE_MANUFACTURER,
-            model=APPLIANCE_MODEL,
-        )
+        self._attr_device_info = monitor.device_info
+        self._attr_unique_id = monitor.unique_id("running")
 
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(self.monitor.add_listener(self.async_write_ha_state))
