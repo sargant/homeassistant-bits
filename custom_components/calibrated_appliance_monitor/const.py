ff47e84459cb563f34b245d8097d48740251f21f
Rob Sargant <robert@sargant.com>
2026-08-17 14:02:05 +0100

    Keep global constants integration-specific

--- custom_components/calibrated_appliance_monitor/const.py
+++ custom_components/calibrated_appliance_monitor/const.py
@@ -3,26 +3,3 @@
 DOMAIN = "calibrated_appliance_monitor"
 CONF_SOURCE_DEVICE = "source_device"
 CONF_ALGORITHM = "algorithm"
-
-ALGORITHM_INDESIT_D2IHL326UK = "indesit_d2ihl326uk"
-
-APPLIANCE_ID = "dishwasher_indesit_d2ihl326uk"
-APPLIANCE_NAME = "Dishwasher"
-APPLIANCE_MANUFACTURER = "Indesit"
-APPLIANCE_MODEL = "D2IHL326UK"
-
-IDLE = "Idle"
-STARTING = "Starting"
-RUNNING = "Running"
-ENDING = "Ending"
-FINISH_PENDING = "Finish pending"
-FINISHED = "Finished"
-RUNNING_STATES = {STARTING, RUNNING, ENDING, FINISH_PENDING}
-
-START_W = 5.0
-ACTIVE_W = 30.0
-ASLEEP_W = 1.0
-START_CONFIRM = 6 * 60
-END_WINDOW = 15 * 60
-FINISH_CONFIRM = 60
-ASLEEP_CONFIRM = 60
